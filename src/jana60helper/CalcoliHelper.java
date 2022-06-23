5a2fd404c4b95a2d6ea9826d2e13ea7dd9d537e5
EmanueleEsposito <manu2022@blu.it>
2022-06-23 15:46:25 +0200

    fine esericzio base

--- src/jana60helper/CalcoliHelper.java
+++ src/jana60helper/CalcoliHelper.java
@@ -2,4 +2,53 @@ package jana60helper;
 
 public class CalcoliHelper {
 
+	private CalcoliHelper() {
+		// costruttore private
+	}
+
+	// Metodi static
+	// Metodo calcolo somma di due numeri interi
+	public static int sommaNumeriInteri(int numero1, int numero2) {
+		return numero1 + numero2;
+	}
+
+	// Metodo differenza tra due numeri interi
+	public static int differenzaNumeriInter(int numero1, int numero2) {
+		return numero1 - numero2;
+	}
+
+	// Metodo moltiplicazione di due numeri interi
+	public static int moltiplicazionneNumeriInteri(int numero1, int numero2) {
+		return numero1 * numero2;
+	}
+
+	// Metodo valore assoluto di un numero intero
+	public static int valoreAssolutoNumeroIntero(int numeroInt) {
+		return (numeroInt < 0) ? -numeroInt : numeroInt;
+	}
+
+	// Metodo minimo tra due numeri interi
+	public static int minimoNumeriIntero(int numero1, int numero2) {
+		int numeroMinimo = 0;
+		if (numero1 < numero2) {
+			return numeroMinimo = numero1;
+		} else if (numero2 < numero1) {
+			return numeroMinimo = numero2;
+		} else
+			numeroMinimo = 0;
+		return numeroMinimo;
+	}
+
+	// Metodo massimo tra due numeri interi
+	int numeroMassimo = 0;
+
+	public static int massimoNumeriIntero(int numero1, int numero2) {
+		int numeroMassimo = 0;
+		if (numero1 > numero2) {
+			return numeroMassimo = numero1;
+		} else if (numero2 > numero1) {
+			return numeroMassimo = numero2;
+		}
+		return numeroMassimo;
+	}
 }
--- src/jana60helper/CalcoliTest.java
+++ src/jana60helper/CalcoliTest.java
@@ -3,7 +3,23 @@ package jana60helper;
 public class CalcoliTest {
 
 	public static void main(String[] args) {
+		// Test somma di due numeri interi
+		System.out.println("La somma è = " + CalcoliHelper.sommaNumeriInteri(5, 10));
 
+		// Test differenza tra due numeri interi
+		System.out.println("La differenza è = " + CalcoliHelper.differenzaNumeriInter(150, 25));
+
+		// Test Moltiplicazione di due numeri interi
+		System.out.println("Il prodotto è = " + CalcoliHelper.moltiplicazionneNumeriInteri(5, 27));
+
+		// Test Valore assoluto di un numero intero
+		System.out.println("Il valore assoluto è = " + CalcoliHelper.valoreAssolutoNumeroIntero(-96));
+
+		// Test Minimo tra due numeri interi
+		System.out.println("Il minimo è = " + CalcoliHelper.minimoNumeriIntero(80, -100));
+
+		// Test Massimo tra due numeri interi
+		System.out.println("Il massimo è = " + CalcoliHelper.massimoNumeriIntero(-150, -125));
 	}
 
 }
